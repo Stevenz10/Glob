86889c464e35b5ee39dafa1d35d1252bf0e11186
Mgchen2013 <66755131+Mgchen2013@users.noreply.github.com>
2020-06-10 17:59:40 -0500

    Add files via upload

---
+++ Ganancias/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Nestle = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.N = New System.Windows.Forms.TextBox()
+        Me.RG = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(163, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ganancias"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(101, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(186, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(120, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(157, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Ganancias compartidas"
+        '
+        'Nestle
+        '
+        Me.Nestle.AutoSize = True
+        Me.Nestle.Location = New System.Drawing.Point(49, 169)
+        Me.Nestle.Name = "Nestle"
+        Me.Nestle.Size = New System.Drawing.Size(48, 17)
+        Me.Nestle.TabIndex = 3
+        Me.Nestle.Text = "Nestle"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(242, 169)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(127, 17)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Otros restaurantes"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(123, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 52)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'N
+        '
+        Me.N.Location = New System.Drawing.Point(12, 213)
+        Me.N.Name = "N"
+        Me.N.Size = New System.Drawing.Size(146, 22)
+        Me.N.TabIndex = 6
+        '
+        'RG
+        '
+        Me.RG.Location = New System.Drawing.Point(233, 213)
+        Me.RG.Name = "RG"
+        Me.RG.Size = New System.Drawing.Size(146, 22)
+        Me.RG.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 320)
+        Me.Controls.Add(Me.RG)
+        Me.Controls.Add(Me.N)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Nestle)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Nestle As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents N As System.Windows.Forms.TextBox
+    Friend WithEvents RG As System.Windows.Forms.TextBox
+
+End Class
---
+++ Ganancias/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim r1, r2, r3, r4 As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        r1 = Val(TextBox1.Text)
+
+        r2 = Val(TextBox1.Text) * 0.65
+        N.Text = r2
+
+        r3 = Val(TextBox1.Text) * 0.35
+        RG.Text= r3
+
+
+
+
+    End Sub
+End Class
